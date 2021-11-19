0942196d8d75e3a1c0b24222fa82e858be04690f
Karol Moroz <k.j.moroz@gmail.com>
2021-11-20 00:37:09 +0100

    Install Absinthe

--- mix.exs
+++ mix.exs
@@ -47,7 +47,9 @@ defmodule PooStorm.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5.3"}
     ]
   end
 
